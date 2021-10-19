6a973df614518df928e1c08744ea062bf467ce9e
Jacob Fenn <jfenn5@uwo.ca>
2021-10-19 14:59:46 -0400

    pushing part of the backend overhaul

--- scripts.js
+++ scripts.js
@@ -1,47 +1,100 @@
-let nextPlayer = '[X]'; // takes a value of either 'X' or 'O' according to the game turns
+let nextPlayer = "X"; // takes a value of either 'X' or 'O' according to the game turns
+let gameboard;
 
 document.getElementById("next-lbl").innerHTML = nextPlayer;
 
 createGameBoard()
 
-function createGameBoard()
-{
+function createGameBoard() {
     for (let i = 1; i < 10; i++) {
-        document.getElementById("c" + i).appendChild(document.createElement("button")).innerHTML = "[ ]";
+        document.getElementById("c" + i).appendChild(document.createElement("button")).innerHTML = " ";
+        document.getElementById("c" + i).firstChild.style.width = "2em";
+        document.getElementById("c" + i).firstChild.style.height = "2em";
+        document.getElementById("c" + i).style.padding = "0px 0px 0px 0px";
     }
 }
 
 // Programatically add 'takeCell' as an event listener to all the buttons on the board
 let btns = document.querySelectorAll('button');
-for (let i=0; i<btns.length; i++)
-{
-    btns[i].addEventListener('click', (event) => { takeCell(event)});
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', (event) => { takeCell(event) });
 }
 
 // This function will be used to respond to a click event on any of the board buttons.
-function takeCell(event)
-{
-    event.path[1].removeChild(event.path[0]);
-    event.path[1].innerHTML = nextPlayer;
-    event.path[1].style.textAlign = 'center';
+function takeCell(event) {
+    // redoing the inplementation here with a js array as the main arbiter
+    // event.path[0].disabled = "disabled";
+    // event.path[1].innerHTML = nextPlayer;
+    // event.path[1].style.textAlign = 'center';
 
-    if(nextPlayer == "[X]") nextPlayer = "[O]";
-    else nextPlayer = "[X]";
+    // document.getElementById("next-lbl").innerHTML = nextPlayer;
 
-    document.getElementById("next-lbl").innerHTML = nextPlayer;
+    // if (isGameOver() && !isGameWon())
+    // {
+    //     document.getElementById("game-over-lbl").innerHTML = "Game Over";
+    // }
+
+    // if(isGameWon(event)) {
+    //     document.getElementById("game-over-lbl").innerHTML = nextPlayer + " won!";
+    // }
+
+    let gameboard = convertGameboard("gameboard");
     
-    if (isGameOver())
-    {
+    event.path[0].disabled = "disabled";
+    event.path[0].innerHTML = nextPlayer;
+
+    let indexes = getIndexOfChange(gameboard, convertGameboard("gameboard"));
+
+    if(isGameWon() != "") {
+        document.getElementById("game-over-lbl").innerHTML = nextPlayer + " won!";
+        endGame();
+    } else if(isGameOver()) {
         document.getElementById("game-over-lbl").innerHTML = "Game Over";
     }
 
-    // could add winning functionality here
+    if (nextPlayer == "X") nextPlayer = "O";
+    else nextPlayer = "X";
+
+
 }
 
-function isGameOver()
-{
-    for (let i = 1; i < 10; i++) {
-        if(document.getElementById("c" + i).firstChild.innerHTML == "[ ]" || document.getElementById("c" + i).innerHTML == "[ ]") return false;
+function isGameOver(gameboard) {
+    for(let j = 0; i < gameboard.length; j++) {
+        if(gameboard[j].contains(" ")) return false;
     }
     return true;
+}
+
+function isGameWon(gameboard) {
+
+}
+
+function endGame() {
+
+}
+
+function convertGameboard(table_id) {
+    myData = document.getElementById(table_id)
+
+    gameboard = []
+    for (var i = 0; i < myData.rows.length; i++) {
+        element = myData.rows[i].children
+        temp = []
+        for (var j = 0; j < element.length; j++) {
+            temp.push(element[j].firstChild.innerText);
+        }
+        gameboard.push(temp);
+
+    }
+    return gameboard;
+}
+
+function getIndexOfChange(preArray, postArray) {
+    for (let i = 0; i < preArray.length; i++) {
+        for (let j = 0; j < preArray[i].length; j++) {
+            if (preArray[i][j] != postArray[i][j]) {
+                return [i, j];
+            }
+        }
+    }
 }
