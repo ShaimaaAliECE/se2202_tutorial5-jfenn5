b32476c6b48cd977d902908e9379b45fe0ff7f68
Jacob Fenn <jfenn5@uwo.ca>
2021-10-18 23:27:29 -0400

    completed tictactoe minus win check

--- scripts.js
+++ scripts.js
@@ -1,17 +1,14 @@
-let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the game turns
+let nextPlayer = '[X]'; // takes a value of either 'X' or 'O' according to the game turns
 
-//initialize the game
+document.getElementById("next-lbl").innerHTML = nextPlayer;
 
-// use the value stored in the nextPlayer variable to indicate who the next player is
-
-
-//This call will create the buttons needed for the gameboard.
 createGameBoard()
 
 function createGameBoard()
 {
-    // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
-   
+    for (let i = 1; i < 10; i++) {
+        document.getElementById("c" + i).appendChild(document.createElement("button")).innerHTML = "[ ]";
+    }
 }
 
 // Programatically add 'takeCell' as an event listener to all the buttons on the board
@@ -24,23 +21,27 @@ for (let i=0; i<btns.length; i++)
 // This function will be used to respond to a click event on any of the board buttons.
 function takeCell(event)
 {
-    /*
-        When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
-    */
+    event.path[1].removeChild(event.path[0]);
+    event.path[1].innerHTML = nextPlayer;
+    event.path[1].style.textAlign = 'center';
 
-    // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
+    if(nextPlayer == "[X]") nextPlayer = "[O]";
+    else nextPlayer = "[X]";
 
-    // Check if the game is over
+    document.getElementById("next-lbl").innerHTML = nextPlayer;
+    
     if (isGameOver())
     {
-        // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+        document.getElementById("game-over-lbl").innerHTML = "Game Over";
     }
 
-    // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
+    // could add winning functionality here
 }
 
 function isGameOver()
 {
-    // This function returns true if all the buttons are disabled and false otherwise 
-   
+    for (let i = 1; i < 10; i++) {
+        if(document.getElementById("c" + i).firstChild.innerHTML == "[ ]" || document.getElementById("c" + i).innerHTML == "[ ]") return false;
+    }
+    return true;
 }
